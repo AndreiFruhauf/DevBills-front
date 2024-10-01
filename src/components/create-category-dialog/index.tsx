fec6e70264b52c239289f02a8e0e14eb20cb34ed
Dinoh665 <fruhaufandrei@gmail.com>
2024-10-01 15:01:28 -0300

    alteração no category-dialog

--- src/components/create-category-dialog/index.tsx
+++ src/components/create-category-dialog/index.tsx
@@ -4,39 +4,41 @@ import { Button } from "../button";
 import { Title } from "../title";
 import { Input } from "../input";
 import { Container } from "./styles";
-import { useFetchAPI } from '../../hooks/useFetchAPI'
+import { useFetchAPI } from '../../hooks/useFetchAPI';
 import { CreateCategoryData } from "../../validators/types";
 
 export function CreateCategoryDialog() {
-    const { createCategory } = useFetchAPI()
-    const [open, setOpen] = useState(false)
+    const { createCategory } = useFetchAPI();
+    const [open, setOpen] = useState(false);
 
     const handleClose = useCallback(() => {
-        setOpen(false)
-    }, [])
+        setOpen(false);
+    }, []);
 
-    const onSubmit = useCallback(() => {
-        async (data: CreateCategoryData) => {
-            await createCategory(data)
-            handleClose()
-        }
-    }, [handleClose, createCategory])
-    
+    const onSubmit = useCallback(async (data: CreateCategoryData) => {
+        await createCategory(data);
+        handleClose();
+    }, [handleClose, createCategory]);
+
     return (
-        <Dialog open={open} onOpenChange={setOpen} trigger={<Button>Nova Categoria</Button>}>
+        <Dialog
+            open={open}
+            onOpenChange={setOpen}
+            trigger={<Button>Nova Categoria</Button>}
+        >
             <Container>
-            <Title title="Nova Categoria" subtitle="Crie uma nova categoria para suas transações"/>
-                <form>
+                <Title title="Nova Categoria" subtitle="Crie uma nova categoria para suas transações" />
+                <form onSubmit={onSubmit}>
                     <div>
-                        <Input label="Nome" placeholder="Nome da categoria"/>
-                        <Input label="Cor" type="color"/>
+                        <Input label="Nome" placeholder="Nome da categoria" />
+                        <Input label="Cor" type="color" />
                     </div>
                     <footer>
-                        <Button onClick={handleClose } variant="outline" type="button">Cancelar</Button>
-                        <Button onClick={onSubmit} type="button">Cadastrar</Button>
+                        <Button onClick={handleClose} variant="outline" type="button">Cancelar</Button>
+                        <Button type="submit">Cadastrar</Button>
                     </footer>
                 </form>
             </Container>
         </Dialog>
-    )
+    );
 }
